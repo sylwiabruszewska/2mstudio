49c4b584dc0c0a9e38631bc6cdde25dbc2905c87
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-22 16:11:45 +0100

    remove keywords

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,12 +8,6 @@ export const Footer = () => {
       <Container className={styles['footer__container']}>
         <div className={styles['box']}>
           <Logo className={styles['logo']} />
-          <h2 className={styles['footer__heading']}>
-            Architekt Białystok, architekt wnętrz Białystok, architektura,
-            projekt domu, projekt wnętrz, nowoczesna architektura, projektowanie
-            przestrzeni, projektowanie mieszkań, wnętrza designerskie, koncepcje
-            aranżacyjne, usługi projektowe Białystok
-          </h2>
         </div>
 
         <div className={styles['box']}>
