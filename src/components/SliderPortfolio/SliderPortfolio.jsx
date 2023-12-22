e5139a3c383574f83f9154351fe93ab0c9743a2a
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-22 02:27:15 +0100

    change lib for SliderPortfolio

--- src/components/SliderPortfolio/SliderPortfolio.jsx
+++ src/components/SliderPortfolio/SliderPortfolio.jsx
@@ -1,61 +1,68 @@
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import styles from './SliderPortfolio.module.scss';
+import { MdArrowForwardIos, MdOutlineArrowBackIos } from 'react-icons/md';
 import image1 from '../../assets/images/slider/interior1.jpg';
 import image2 from '../../assets/images/slider/interior2.jpg';
 import image3 from '../../assets/images/slider/interior3.jpg';
 import image4 from '../../assets/images/slider/interior4.jpg';
 
 export const SliderPortfolio = () => {
+  const PrevArrow = ({ onClick }) => (
+    <div className={styles['prev-arrow-custom']} onClick={onClick}>
+      <MdOutlineArrowBackIos className={styles['arrow-icon']} />
+    </div>
+  );
+
+  const NextArrow = ({ onClick }) => (
+    <div className={styles['next-arrow-custom']} onClick={onClick}>
+      <MdArrowForwardIos className={styles['arrow-icon']} />
+    </div>
+  );
+
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
+
   return (
-    <Carousel
-      additionalTransfrom={0}
-      arrows
-      autoPlay
-      autoPlaySpeed={3000}
-      centerMode={false}
-      containerClass=""
-      customButtonGroup={null}
-      dotListClass=""
-      draggable
-      focusOnSelect={false}
-      infinite
-      keyBoardControl
-      minimumTouchDrag={80}
-      renderButtonGroupOutside={false}
-      renderDotsOutside={false}
-      responsive={{
-        desktop: {
-          breakpoint: {
-            max: 3000,
-            min: 1280,
-          },
-          items: 3,
-        },
-        tablet: {
-          breakpoint: {
-            max: 1280,
-            min: 768,
-          },
-          items: 2,
-        },
-        mobile: {
-          breakpoint: {
-            max: 768,
-            min: 0,
-          },
-          items: 1,
-        },
-      }}
-      sliderClass=""
-    >
-      <img src={image1} className={styles['slider-img']} alt="Opis obrazka 1" />
-      <img src={image2} className={styles['slider-img']} alt="Opis obrazka 2" />
-      <img src={image3} className={styles['slider-img']} alt="Opis obrazka 3" />
-      <img src={image4} className={styles['slider-img']} alt="Opis obrazka 4" />
-      <img src={image1} className={styles['slider-img']} alt="Opis obrazka 1" />
-      <img src={image2} className={styles['slider-img']} alt="Opis obrazka 2" />
-    </Carousel>
+    <Slider {...settings} className={styles['carousel-wrapper']}>
+      <div>
+        <img
+          src={image1}
+          className={styles['slider-img']}
+          alt="Opis obrazka 1"
+        />
+      </div>
+      <div>
+        <img
+          src={image2}
+          className={styles['slider-img']}
+          alt="Opis obrazka 2"
+        />
+      </div>
+      <div>
+        <img
+          src={image3}
+          className={styles['slider-img']}
+          alt="Opis obrazka 3"
+        />
+      </div>
+      <div>
+        <img
+          src={image4}
+          className={styles['slider-img']}
+          alt="Opis obrazka 4"
+        />
+      </div>
+    </Slider>
   );
 };
