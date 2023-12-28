3b10829174c9aa476ca6ed8e7dfa562f40166177
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-12-28 14:35:50 +0100

    add checkbox for form, set messages on submit and error

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,17 +1,25 @@
 import axios from 'axios';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import { useState } from 'react';
 
 import styles from './ContactForm.module.scss';
 import { Button } from 'components';
 
 export const ContactForm = () => {
-  const handleSubmit = async (values, { setSubmitting }) => {
+  const [submitted, setSubmitted] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
     try {
       const res = await axios.post('/send_email.php', values);
       console.dir(res);
+      setSubmitted(true);
+      errorMessage(false);
+      resetForm();
     } catch (error) {
       console.error(error);
+      setErrorMessage(true);
     } finally {
       setSubmitting(false);
     }
@@ -35,95 +43,134 @@ export const ContactForm = () => {
       )
       .min(50, 'Wiadomość musi zawierać co najmniej 50 znaków.')
       .required('Wiadomość jest wymagana.'),
+    consent: Yup.boolean()
+      .oneOf([true], 'Zgoda na przetwarzanie danych jest wymagana')
+      .required('Zgoda na przetwarzanie danych jest wymagana'),
   });
 
   return (
-    <Formik
-      initialValues={{
-        name: '',
-        email: '',
-        phone: '',
-        message: '',
-      }}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      {({ isSubmitting }) => (
-        <Form className={styles['form']}>
-          <h3 className={styles['form-heading']}>Formularz kontaktowy</h3>
-          <label>
-            Imię:
-            <Field
-              className={styles['form__input']}
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Imię"
-            />
-            <ErrorMessage
-              name="name"
-              component="div"
-              className={styles['error-message']}
-            />
-          </label>
-
-          <label>
-            Adres e-mail:
-            <Field
-              className={styles['form__input']}
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Adres e-mail"
-            />
-            <ErrorMessage
-              name="email"
-              component="div"
-              className={styles['error-message']}
-            />
-          </label>
-
-          <label>
-            Numer telefonu:
-            <Field
-              className={styles['form__input']}
-              type="text"
-              name="phone"
-              id="phone"
-              placeholder="Numer telefonu"
-            />
-            <ErrorMessage
-              name="phone"
-              component="div"
-              className={styles['error-message']}
-            />
-          </label>
-
-          <label>
-            Wpisz wiadomość:
-            <Field
-              className={styles['form__textarea']}
-              as="textarea"
-              name="message"
-              id="message"
-              placeholder="Wpisz wiadomość"
-            />
-            <ErrorMessage
-              name="message"
-              component="div"
-              className={styles['error-message']}
-            />
-          </label>
-
-          <Button
-            className={styles['button']}
-            type="submit"
-            disabled={isSubmitting}
+    <>
+      {!submitted ? (
+        <>
+          <Formik
+            initialValues={{
+              name: '',
+              email: '',
+              phone: '',
+              message: '',
+              consent: false,
+            }}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
           >
-            Wyślij
-          </Button>
-        </Form>
+            {({ isSubmitting }) => (
+              <Form className={styles['form']}>
+                <h3 className={styles['form-heading']}>Formularz kontaktowy</h3>
+                <label>
+                  Imię:
+                  <Field
+                    className={styles['form__input']}
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Imię"
+                  />
+                  <ErrorMessage
+                    name="name"
+                    component="div"
+                    className={styles['error-message']}
+                  />
+                </label>
+
+                <label>
+                  Adres e-mail:
+                  <Field
+                    className={styles['form__input']}
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Adres e-mail"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className={styles['error-message']}
+                  />
+                </label>
+
+                <label>
+                  Numer telefonu:
+                  <Field
+                    className={styles['form__input']}
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    placeholder="Numer telefonu"
+                  />
+                  <ErrorMessage
+                    name="phone"
+                    component="div"
+                    className={styles['error-message']}
+                  />
+                </label>
+
+                <label>
+                  Wpisz wiadomość:
+                  <Field
+                    className={styles['form__textarea']}
+                    as="textarea"
+                    name="message"
+                    id="message"
+                    placeholder="Wpisz wiadomość"
+                  />
+                  <ErrorMessage
+                    name="message"
+                    component="div"
+                    className={styles['error-message']}
+                  />
+                </label>
+
+                <label className={styles['checkbox-label']}>
+                  <Field
+                    type="checkbox"
+                    name="consent"
+                    className={styles['form__checkbox']}
+                  />
+                  <span className={styles['checkbox-text']}>
+                    Zgadzam się na wykorzystanie moich danych osobowych przez
+                    firmę 2M STUDIO Pracownia Projektowa w celu skontaktowania
+                    się ze mną i udzielenia odpowiedzi na moje zapytanie.
+                  </span>
+                  <ErrorMessage
+                    name="consent"
+                    component="div"
+                    className={styles['error-message']}
+                  />
+                </label>
+
+                <Button
+                  className={styles['button']}
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Wyślij
+                </Button>
+              </Form>
+            )}
+          </Formik>
+          {errorMessage && (
+            <div>
+              Wystąpił błąd podczas wysyłania wiadomości. Spróbuj ponownie.
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          {submitted && (
+            <div>Dziękujemy za wiadomość, skontaktujemy się z Państwem.</div>
+          )}
+        </>
       )}
-    </Formik>
+    </>
   );
 };
