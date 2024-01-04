96c149210c55c618443a431a3a33f85a0d4827a4
sylwiabruszewska <53376938+sylwiabruszewska@users.noreply.github.com>
2024-01-04 11:36:42 +0000

    Deploying to gh-pages from @ sylwiabruszewska/2mstudio@7f343ab13ed6ff0bd6a98ba7084ee10e74a8a6e4 🚀

--- static/js/794.b3acc281.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_2mstudio=self.webpackChunk_2mstudio||[]).push([[794],{1794:(e,n,i)=>{i.r(n),i.d(n,{default:()=>r});var s=i(4270),t=i(184);const r=()=>(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)(s.q,{children:(0,t.jsx)("title",{children:"2m Studio Pracownia Projektowa - Strony nie znaleziono"})}),(0,t.jsx)("p",{children:"404 - Ups! Strony nie znaleziono"}),(0,t.jsx)("p",{children:"Strona, kt\xf3rej szukasz nie istnieje."})]})}}]);
-//# sourceMappingURL=794.b3acc281.chunk.js.map
