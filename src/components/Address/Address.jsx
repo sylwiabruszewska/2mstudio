7198798b92a8291a517bedbc26ae781e4ba48c9c
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-01-04 13:24:35 +0100

    update Address component - display options

--- src/components/Address/Address.jsx
+++ src/components/Address/Address.jsx
@@ -1,48 +1,52 @@
-import { FaEnvelope } from 'react-icons/fa';
-import { FaPhoneAlt } from 'react-icons/fa';
-import { FaHome } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
+import { FaEnvelope, FaPhoneAlt, FaHome } from 'react-icons/fa';
 import styles from './Address.module.scss';
 
-export const Address = () => {
+export const Address = ({ contain }) => {
+  const addressOptions = {
+    tel: {
+      icon: <FaPhoneAlt className={styles.icon} />,
+      text: '+48 604 310 260',
+      link: 'tel:+48604310260',
+    },
+    email: {
+      icon: <FaEnvelope className={styles.icon} />,
+      text: 'marczak@2mstudio.org.pl',
+      link: 'mailto:marczak@2mstudio.org.pl',
+    },
+    location: {
+      icon: <FaHome className={styles.icon} />,
+      text: 'Mikołaja Gogola 1, 15-166 Białystok',
+      link: 'https://www.google.com/maps/place/2M+Studio+-+Pracownia+Projektowa/@53.1499476,23.2081865,17z/data=!4m6!3m5!1s0x471fffd2ad529349:0x1be59594f6b226c9!8m2!3d53.151338!4d23.2078297!16s%2Fg%2F11jzgkfr3g?entry=ttu',
+    },
+  };
+
+  const displayOptions = contain ? [contain] : ['tel', 'email', 'location'];
+
   return (
     <address>
       <ul className={styles['address__list']}>
-        <li className={styles['address__item']}>
-          <div className={styles['list__content']}>
-            <FaPhoneAlt className={styles.icon} />
-            <a className={styles['address__link']} href="tel:+48604310260">
-              +48 604 310 260
-            </a>
-          </div>
-        </li>
-
-        <li className={styles['address__item']}>
-          <div className={styles['list__content']}>
-            <FaEnvelope className={styles.icon} />
-            <a
-              className={styles['address__link']}
-              href="mailto:marczak@2mstudio.org.pl"
-            >
-              marczak@2mstudio.org.pl
-            </a>
-          </div>
-        </li>
-
-        <li className={styles['address__item']}>
-          <div className={styles['list__content']}>
-            <FaHome className={styles.icon} />
-            <a
-              className={styles['address__link']}
-              href="https://www.google.com/maps/place/2M+Studio+-+Pracownia+Projektowa/@53.1499476,23.2081865,17z/data=!4m6!3m5!1s0x471fffd2ad529349:0x1be59594f6b226c9!8m2!3d53.151338!4d23.2078297!16s%2Fg%2F11jzgkfr3g?entry=ttu"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Mikołaja Gogola 1, 15-166 Białystok
-            </a>
-          </div>
-        </li>
+        {displayOptions.map(option => (
+          <li key={option} className={styles['address__item']}>
+            <div className={styles['list__content']}>
+              {addressOptions[option].icon}
+              <a
+                className={styles['address__link']}
+                href={addressOptions[option].link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {addressOptions[option].text}
+              </a>
+            </div>
+          </li>
+        ))}
       </ul>
     </address>
   );
+};
+
+Address.propTypes = {
+  contain: PropTypes.oneOf(['tel', 'email', 'location']),
 };
