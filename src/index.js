2ed89dda037721687b762023ccdcd9f48105859e
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-01-09 15:48:33 +0100

    add react query options

--- src/index.js
+++ src/index.js
@@ -6,13 +6,28 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 24 * 60 * 60 * 1000,
+      staleTime: 60 * 60 * 1000,
+      // refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename="/2mstudio">
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
