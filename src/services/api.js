7027dc12b55a538eee6bdf102d35a739ea8c906a
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-01-09 16:40:23 +0100

    refactor api functions - minify return data, cleaning

--- src/services/api.js
+++ src/services/api.js
@@ -10,6 +10,7 @@ const instance = axios.create({
   },
 });
 
+// fetch data
 export const fetchData = async path => {
   try {
     const response = await instance.get(`${BASE_URL}${path}`, {
@@ -29,6 +30,60 @@ export const fetchData = async path => {
   }
 };
 
+// fetch projects with images
+export const fetchProjectsWithImages = async path => {
+  const projects = await fetchData(path);
+
+  const projectsWithImages = await Promise.all(
+    projects.map(async project => {
+      let simplifiedProject = {
+        id: project.id,
+        featured_media: defaultImage,
+        title: project.title.rendered,
+      };
+
+      if (project.featured_media) {
+        try {
+          const featuredMedia = await fetchImages(project.featured_media);
+          simplifiedProject.featured_media =
+            featuredMedia.source_url || defaultImage;
+        } catch (error) {
+          console.error(
+            'There was a problem fetching images for project:',
+            error
+          );
+        }
+      }
+
+      return simplifiedProject;
+    })
+  );
+
+  return projectsWithImages;
+};
+
+// fetch projects for different categories
+export const getPortfolioInterior = async () => {
+  const projects = await fetchProjectsWithImages(API_PATHS.portfolioInterior);
+  return projects;
+};
+
+export const getPortoflioBuildingsResidential = async () => {
+  const projects = await fetchData(API_PATHS.portoflioBuildingsResidential);
+  return projects;
+};
+
+export const getPortoflioBuildingsIndustrial = async () => {
+  const projects = await fetchData(API_PATHS.portoflioBuildingsIndustrial);
+  return projects;
+};
+
+export const getPortoflioBuildingsCommercial = async () => {
+  const projects = await fetchData(API_PATHS.portoflioBuildingsCommercial);
+  return projects;
+};
+
+// fetch blog posts - to update
 export const getPost = async postId => {
   const post = await fetchData(`${API_PATHS.post}/${postId}`);
   return post;
@@ -59,49 +114,7 @@ export const getBlogPosts = async (page, perPage = 6) => {
   }
 };
 
-// export const getPortfolioInterior = async () => {
-//   const projects = await fetchData(API_PATHS.portfolioInterior);
-//   return projects;
-// };
-
-export const getPortfolioInterior = async () => {
-  const projects = await fetchData(API_PATHS.portfolioInterior);
-
-  const projectsWithImages = await Promise.all(
-    projects.map(async project => {
-      if (project.featured_media) {
-        try {
-          const featuredMedia = await fetchImages(project.featured_media);
-          project.featured_media = featuredMedia?.source_url || defaultImage;
-        } catch (error) {
-          console.error(
-            'There was a problem fetching images for project:',
-            error
-          );
-        }
-      }
-      return project;
-    })
-  );
-
-  return projectsWithImages;
-};
-
-export const getPortoflioBuildingsResidential = async () => {
-  const projects = await fetchData(API_PATHS.portoflioBuildingsResidential);
-  return projects;
-};
-
-export const getPortoflioBuildingsIndustrial = async () => {
-  const projects = await fetchData(API_PATHS.portoflioBuildingsIndustrial);
-  return projects;
-};
-
-export const getPortoflioBuildingsCommercial = async () => {
-  const projects = await fetchData(API_PATHS.portoflioBuildingsCommercial);
-  return projects;
-};
-
+// fetch data for a subpage - About Us
 export const getAboutUsInfo = async () => {
   const data = await fetchData(API_PATHS.aboutUs);
   if (data && data.content && data.content.rendered) {
@@ -111,6 +124,7 @@ export const getAboutUsInfo = async () => {
   }
 };
 
+// fetch images for projects, blog posts
 export const fetchImages = async mediaId => {
   try {
     const response = await instance.get(`${API_PATHS.media}${mediaId}`);
