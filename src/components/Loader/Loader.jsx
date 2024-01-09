6a5af368307739141d7775dd1b87a18e8f354094
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-01-09 20:49:37 +0100

    change loader style

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,5 @@
-import { TailSpin } from 'react-loader-spinner';
-
 import styles from './Loader.module.scss';
 
 export const Loader = () => {
-  return (
-    <div className={styles['loader']}>
-      <TailSpin
-        height="80"
-        width="80"
-        color="#c0c0c0"
-        ariaLabel="tail-spin-loading"
-        radius="1"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-      />
-    </div>
-  );
+  return <div className={styles['loader']}></div>;
 };
