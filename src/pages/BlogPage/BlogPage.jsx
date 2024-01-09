221c498751cfa2268ad66d63af356293acbd151a
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-01-09 19:36:41 +0100

    refactor BlogPage - useQuery

--- src/pages/BlogPage/BlogPage.jsx
+++ src/pages/BlogPage/BlogPage.jsx
@@ -1,30 +1,37 @@
 import { Helmet } from 'react-helmet';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { useQuery } from 'react-query';
 
 import styles from './BlogPage.module.scss';
-import { Section, Container, Button, AnimatedRoute } from 'components';
-import { getImages } from '../../helpers/getImages';
+import { Section, Container, Button, Loader } from 'components';
 import { getBlogPosts } from '../../services/api';
 
 const BlogPage = () => {
-  const [posts, setPosts] = useState();
-  const [featuredImages, setFeaturedImages] = useState({});
   const [currentPage, setCurrentPage] = useState(1);
-  const [lastPageBlog, setLastPageBlog] = useState(null);
   const location = useLocation();
-  const [dataLoaded, setDataLoaded] = useState(false);
-
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage]);
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
     const page = parseInt(queryParams.get('page') || '1');
     setCurrentPage(page);
   }, [location]);
+
+  const { data, isLoading, isError } = useQuery(
+    ['blogPosts', currentPage],
+    () => getBlogPosts(currentPage)
+  );
+
+  const posts = data?.posts;
+  const lastPage = data?.lastPage;
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <p>Wystąpił błąd, spróbuj odświeżyć stronę.</p>;
+  }
 
   const goToNextPage = () => {
     const nextPage = currentPage + 1;
@@ -41,30 +48,11 @@ const BlogPage = () => {
   const updateURL = page => {
     const queryParams = new URLSearchParams(location.search);
     queryParams.set('page', page);
-    const newURL = `${location.pathname}?${queryParams.toString()}`;
+    const newURL = `${process.env.PUBLIC_URL}${
+      location.pathname
+    }?${queryParams.toString()}`;
     window.history.pushState({}, '', newURL);
   };
-
-  const fetchData = async () => {
-    try {
-      const { posts, lastPage } = await getBlogPosts(currentPage);
-
-      setPosts(posts);
-
-      if (lastPageBlog === null) {
-        setLastPageBlog(lastPage);
-      }
-      setDataLoaded(true);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  useEffect(() => {
-    if (posts && posts.length > 0) {
-      getImages(posts, featuredImages, setFeaturedImages);
-    }
-  }, [posts, featuredImages]);
 
   return (
     <>
@@ -74,75 +62,60 @@ const BlogPage = () => {
 
       <Section>
         <Container>
-          {dataLoaded && (
-            <AnimatedRoute>
-              <div className={styles['blog-list']}>
-                {posts &&
-                  posts.map(post => (
-                    <article key={post.id} className={styles['blog-card']}>
-                      <div className={styles['blog-post']}>
-                        <Link to={`/${post.id}`}>
-                          <p className={styles['blog-date']}>
-                            {new Date(post.date).toLocaleDateString('pl-PL', {
-                              year: 'numeric',
-                              month: 'long',
-                              day: 'numeric',
-                            })}
-                          </p>
-                          <h2 className={styles['blog-title']}>
-                            {post.title.rendered}
-                          </h2>
-                          <p
-                            className="blog-excerpt"
-                            dangerouslySetInnerHTML={{
-                              __html: post.excerpt.rendered,
-                            }}
-                          />
-                        </Link>
-                      </div>
-                      <div className={styles['overlay']}>
-                        <Link to={`/${post.id}`}>
-                          {post.featured_media !== 0 &&
-                            featuredImages[post.featured_media] && (
-                              <div>
-                                <img
-                                  src={featuredImages[post.featured_media]}
-                                  alt={post.title.rendered}
-                                  className={styles['post-img']}
-                                />
+          <div className={styles['blog-list']}>
+            {posts &&
+              posts.map(post => (
+                <article key={post.id} className={styles['blog-card']}>
+                  <div className={styles['blog-post']}>
+                    <Link to={`/${post.id}`}>
+                      <p className={styles['blog-date']}>
+                        {new Date(post.date).toLocaleDateString('pl-PL', {
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric',
+                        })}
+                      </p>
+                      <h2 className={styles['blog-title']}>{post.title}</h2>
+                      <p
+                        className="blog-excerpt"
+                        dangerouslySetInnerHTML={{
+                          __html: post.excerpt,
+                        }}
+                      />
+                    </Link>
+                  </div>
+                  <div className={styles['overlay']}>
+                    <Link to={`/${post.id}`}>
+                      <div>
+                        <img
+                          src={post.img}
+                          alt={post.title}
+                          className={styles['post-img']}
+                        />
 
-                                <div className={styles['box']}>zobacz post</div>
-                              </div>
-                            )}
-                        </Link>
+                        <div className={styles['box']}>zobacz post</div>
                       </div>
-                    </article>
-                  ))}
-              </div>
-              <div className={styles['btn-group']}>
-                <div>
-                  {posts && currentPage > 1 && (
-                    <Button
-                      className={styles['btn-pag']}
-                      onClick={goToPrevPage}
-                    >
-                      Poprzednia strona
-                    </Button>
-                  )}
-                </div>
-                <div>
-                  {currentPage < lastPageBlog && posts && (
-                    <Button
-                      className={styles['btn-pag']}
-                      onClick={goToNextPage}
-                    >
-                      Następna strona
-                    </Button>
-                  )}
-                </div>
-              </div>
-            </AnimatedRoute>
-          )}
+                    </Link>
+                  </div>
+                </article>
+              ))}
+          </div>
+          <div className={styles['btn-group']}>
+            <div>
+              {posts && currentPage > 1 && (
+                <Button className={styles['btn-pag']} onClick={goToPrevPage}>
+                  Poprzednia strona
+                </Button>
+              )}
+            </div>
+            <div>
+              {currentPage < lastPage && posts && (
+                <Button className={styles['btn-pag']} onClick={goToNextPage}>
+                  Następna strona
+                </Button>
+              )}
+            </div>
+          </div>
         </Container>
       </Section>
     </>
