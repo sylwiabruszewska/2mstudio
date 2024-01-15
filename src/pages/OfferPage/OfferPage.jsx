155cf308c1bd4d5c122be2161b9fe1b7673e1637
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-01-15 17:31:00 +0100

    update OfferPage - add request to api and useQuery, update styles

--- src/pages/OfferPage/OfferPage.jsx
+++ src/pages/OfferPage/OfferPage.jsx
@@ -1,9 +1,22 @@
 import { Helmet } from 'react-helmet';
+import parse from 'html-react-parser';
+import { useQuery } from 'react-query';
 
-// import styles from './OfferPage.module.scss';
-import { Section, Container, BackLink } from 'components';
+import styles from './OfferPage.module.scss';
+import { Section, Container, BackLink, Loader } from 'components';
+import { getOfferData } from '../../services/api';
 
 const OfferPage = () => {
+  const { data, isLoading, isError } = useQuery('offerData', getOfferData);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <p>Wystąpił błąd, spróbuj odświeżyć stronę.</p>;
+  }
+
   return (
     <>
       <Helmet>
@@ -12,7 +25,10 @@ const OfferPage = () => {
 
       <Section>
         <Container>
-          <div>Oferta</div>
+          <div className={styles['wrapper']}>
+            <div className={styles['content']}>{data && parse(data)}</div>
+          </div>
+
           <BackLink />
         </Container>
       </Section>
